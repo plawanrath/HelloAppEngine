639d43aa8740e33f2311b8697ef668b7296150a3
Plawan Rath <plawanrath@gmail.com>
2018-10-12 21:18:04 -0400

    email test

--- src/main/java/com/example/HelloAppEngine/HelloAppEngineApplication.java
+++ src/main/java/com/example/HelloAppEngine/HelloAppEngineApplication.java
@@ -6,6 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Properties;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
 @SpringBootApplication
 @RestController
 public class HelloAppEngineApplication {
@@ -22,5 +28,24 @@ public class HelloAppEngineApplication {
 	@GetMapping("/name/{name}")
 	public String name(@PathVariable String name) {
 		return "My name is " + name;
+	}
+
+	@GetMapping("/email/{from}/{to}/{subject}/{message}")
+	public String email(@PathVariable String from, @PathVariable String to, @PathVariable String subject, @PathVariable String message) {
+		String res;
+		Properties props = new Properties();
+		Session session = Session.getDefaultInstance(props, null);
+		try {
+			Message msg = new MimeMessage(session);
+			msg.setFrom(new InternetAddress(from));
+			msg.addRecipient(Message.RecipientType.TO,
+							 new InternetAddress(to));
+			msg.setSubject(subject);
+			msg.setText(message);
+			res = "Message was sent!!";
+		} catch(Exception e) {
+			res = e.getMessage();
+		}
+		return res;
 	}
 }
