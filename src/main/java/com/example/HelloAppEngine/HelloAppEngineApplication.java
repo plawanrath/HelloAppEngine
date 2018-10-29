9322b1a757ea0e671d68d0301d9cdb15339e91e9
Plawan Rath <plawanrath@gmail.com>
2018-10-29 17:10:06 -0400

    adding changes

--- src/main/java/com/example/HelloAppEngine/HelloAppEngineApplication.java
+++ src/main/java/com/example/HelloAppEngine/HelloAppEngineApplication.java
@@ -6,6 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.google.api.client.repackaged.org.apache.commons.codec.binary.Base64;
+import com.google.api.services.gmail.Gmail;
+import com.google.api.services.gmail.model.Message;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.Properties;
 
 import javax.mail.*;
@@ -30,22 +36,46 @@ public class HelloAppEngineApplication {
 		return "My name is " + name;
 	}
 
-	@GetMapping("/email/{from}/{to}/{subject}/{message}")
-	public String email(@PathVariable String from, @PathVariable String to, @PathVariable String subject, @PathVariable String message) {
-		String res;
-		Properties props = new Properties();
-		Session session = Session.getDefaultInstance(props, null);
-		try {
-			Message msg = new MimeMessage(session);
-			msg.setFrom(new InternetAddress(from));
-			msg.addRecipient(Message.RecipientType.TO,
-							 new InternetAddress(to));
-			msg.setSubject(subject);
-			msg.setText(message);
-			res = "Message was sent!!";
-		} catch(Exception e) {
-			res = e.getMessage();
-		}
-		return res;
+	@GetMapping("{/email/subject}/{message}")
+	public String email(Gmail service,@PathVariable String subject, @PathVariable String message) throws Exception {
+		String from = "plawanrath@gmail.com";
+		String to = "plawanrath@gmail.com";
+		MimeMessage msg = createEmail(to, from, subject, message);
+		Message gmsg = createMessageWithEmail(msg);
+		gmsg = service.users().messages().send(to, gmsg).execute();
+		return gmsg.getRaw();
 	}
+
+	public MimeMessage createEmail(String to, String from, String subject, String body) throws MessagingException {
+        Properties props = new Properties();
+        Session session = Session.getDefaultInstance(props, null);
+
+        MimeMessage email = new MimeMessage(session);
+
+        email.setFrom(new InternetAddress(from));
+        email.addRecipient(javax.mail.Message.RecipientType.TO,
+                new InternetAddress(to));
+        email.setSubject(subject);
+        email.setText(body);
+        return email;
+	}
+
+    /**
+     * Create a message from an email.
+     *
+     * @param emailContent Email to be set to raw of message
+     * @return a message containing a base64url encoded email
+     * @throws IOException
+     * @throws MessagingException
+     */
+    public Message createMessageWithEmail(MimeMessage emailContent)
+            throws MessagingException, IOException {
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        emailContent.writeTo(buffer);
+        byte[] bytes = buffer.toByteArray();
+        String encodedEmail = Base64.encodeBase64URLSafeString(bytes);
+        Message message = new Message();
+        message.setRaw(encodedEmail);
+        return message;
+    }	
 }
